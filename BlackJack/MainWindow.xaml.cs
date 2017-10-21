e07104102f8b242bb299a3ddd68fa55a6a48e0d3
CC <261909098@qq.com>
2017-10-22 01:20:35 +0800

    继续瞎改改

--- BlackJack/MainWindow.xaml.cs
+++ BlackJack/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,32 @@ namespace BlackJack
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void Exit_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+
+		private void Help_Click(object sender, RoutedEventArgs e)
+		{
+			Process.Start("https://baike.baidu.com/item/21点/5481683");
+		}
+
+		private void About_Click(object sender, RoutedEventArgs e)
+		{
+			var window = new AboutWindow()
+			{
+				Owner = this,
+			};
+			window.ShowDialog();
+		}
+
+		protected override void OnClosing(CancelEventArgs e)
+		{
+			var result = MessageBox.Show("确定要退出吗？", "BlackJack", MessageBoxButton.YesNo);
+			if (result == MessageBoxResult.No)
+				e.Cancel = true;
 		}
 	}
 }
